f4599cc2a8fd60cca55af35c123f1ca1e97f721e
Viktor Penelski <viktor.penelski@paysafe.com>
2016-09-14 14:30:07 +0300

    [INT-613] Changed all return types from TasksRestController and ProjectsRestController from regular string to json object.toString()

--- src/main/java/paysafe/interns/controllers/ProjectsRestController.java
+++ src/main/java/paysafe/interns/controllers/ProjectsRestController.java
@@ -3,7 +3,6 @@ package paysafe.interns.controllers;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
@@ -35,205 +34,227 @@ import java.util.stream.Collectors;
  */
 @RestController
 public class ProjectsRestController {
-	@Autowired
-	private ProjectsRepository projectsRepository;
+    @Autowired
+    private ProjectsRepository projectsRepository;
 
-	@Autowired
-	private TasksRepository tasksRepository;
+    @Autowired
+    private TasksRepository tasksRepository;
 
-	/**
-	 * GET Serving {@link ProjectsRepository#findAll()} method
-	 * 
-	 * @return a json string array of all projects or empty array
-	 */
-	// TODO: Should return all projects created by a user
-	@RequestMapping("/projects")
-	Iterable<Project> getAllProjects() {
-		return this.projectsRepository.findAll();
-	}
+    /**
+     * GET Serving {@link ProjectsRepository#findAll()} method
+     *
+     * @return a json string array of all projects or empty array
+     */
+    // TODO: Should return all projects created by a user
+    @RequestMapping("/projects")
+    Iterable<Project> getAllProjects() {
+        return this.projectsRepository.findAll();
+    }
 
-	/**
-	 * GET Serving the {@link ProjectsRepository#findOne(Serializable)} method
-	 * 
-	 * @param id
-	 *            a path parameter by which the method finds a specific project
-	 * @return a json representation of a project or empty string
-	 */
-	@RequestMapping("/project/{id}")
-	Project getProjectById(@PathVariable Long id) {
-		Project project = this.projectsRepository.findOne(id);
-		if (project == null) {
-			throw new InvalidProjectException(String.format("Unable to find task with id %d", id));
-		}
-		return this.projectsRepository.findOne(id);
-	}
+    /**
+     * GET Serving the {@link ProjectsRepository#findOne(Serializable)} method
+     *
+     * @param id a path parameter by which the method finds a specific project
+     * @return a json representation of a project or empty string
+     */
+    @RequestMapping("/project/{id}")
+    Project getProjectById(@PathVariable Long id) {
+        Project project = this.projectsRepository.findOne(id);
+        if (project == null) {
+            throw new InvalidProjectException(String.format("Unable to find task with id %d", id));
+        }
+        return this.projectsRepository.findOne(id);
+    }
 
-	/**
-	 * POST Serves the {@link ProjectsRepository#save(Object)} method. Saves a
-	 * project in the database
-	 * 
-	 * @param project
-	 *            the project to be saved which is passed by a json body request
-	 *            parameter
-	 * @return json string of an anonymous object holding the id of the created
-	 *         project
-	 * @throws InvalidProjectException
-	 *             thrown when the JPA throws a
-	 *             {@link javax.validation.ConstraintViolationException} when
-	 *             validating the input json request body.
-	 */
-	@RequestMapping(value = "/projects", method = RequestMethod.POST)
-	String addProject(@RequestBody Project project) throws InvalidProjectException {
-		try {
-			this.projectsRepository.save(project);
-		} catch (ConstraintViolationException cve) {
-			throw new InvalidProjectException(cve);
-		}
+    /**
+     * POST Serves the {@link ProjectsRepository#save(Object)} method. Saves a
+     * project in the database
+     *
+     * @param project the project to be saved which is passed by a json body request
+     *                parameter
+     * @return json string of an anonymous object holding the id of the created
+     * project
+     * @throws InvalidProjectException thrown when the JPA throws a
+     *                                 {@link javax.validation.ConstraintViolationException} when
+     *                                 validating the input json request body.
+     */
+    @RequestMapping(value = "/projects", method = RequestMethod.POST)
+    String addProject(@RequestBody Project project) throws InvalidProjectException {
+        try {
+            this.projectsRepository.save(project);
+        } catch (ConstraintViolationException cve) {
+            throw new InvalidProjectException(cve);
+        }
 
-		JSONObject response = new JSONObject();
-		try {
-			response.put("id", project.getId());
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return response.toString();
-	}
+        JSONObject response = new JSONObject();
+        try {
+            response.put("id", project.getId());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return response.toString();
+    }
 
-	/**
-	 * DELETE Serving {@link ProjectsRepository}
-	 * method. First get all tasks related to the provided project id and
-	 * deleted tasks.Then delete current project.
-	 * 
-	 * @param projectId
-	 *            a path parameter by which the method finds a specific progect
-	 * @return Project with current projectId deleted or Unable to find project
-	 *         with current projectId.
-	 */
-	@RequestMapping(value = "/project/{projectId}", method = RequestMethod.DELETE)
-	public String deleteProjectByProjectId(@PathVariable Long projectId) {
-		try {
-			Project projectForDeleted = projectsRepository.getOne(projectId);
-			Iterable<Task> tasksByCurrentProject = tasksRepository.findAllByProjectId(projectId);
-			for (Task task : tasksByCurrentProject) {
-				tasksRepository.delete(task);
-			}
+    /**
+     * DELETE Serving {@link ProjectsRepository}
+     * method. First get all tasks related to the provided project id and
+     * deleted tasks.Then delete current project.
+     *
+     * @param projectId a path parameter by which the method finds a specific progect
+     * @return Project with current projectId deleted or Unable to find project
+     * with current projectId.
+     */
+    @RequestMapping(value = "/project/{projectId}", method = RequestMethod.DELETE)
+    public String deleteProjectByProjectId(@PathVariable Long projectId) {
+        if (projectsRepository.exists(projectId)) {
+            Project projectForDeleted = projectsRepository.getOne(projectId);
+            Iterable<Task> tasksByCurrentProject = tasksRepository.findAllByProjectId(projectId);
+            for (Task task : tasksByCurrentProject) {
+                tasksRepository.delete(task);
+            }
+            projectsRepository.delete(projectForDeleted);
+            JSONObject response = new JSONObject();
+            try {
+                response.put("id", projectId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return response.toString();
+        } else {
+            throw new InvalidProjectException("Project does not exists");
+        }
+    }
 
-			projectsRepository.delete(projectForDeleted);
-
-			return "Project " + projectId + " deleted.";
-
-		} catch (JpaObjectRetrievalFailureException jorfe) {
-			return "Unable to find project with id " + projectId;
-		}
-	}
-
-	/**
-	 * PATCH Serving {@link ProjectsRepository} method.
-	 * Saves a given file in the project.
-	 * @param projectId
-	 * 				the project in which the file will be saved.
-	 * @param multipartFile
-	 * 				the file that will be saved in the project.
-	 * 				The file should have a valid content type {@link #fileContentTypeIsAllowed(String)}
+    /**
+     * PATCH Serving {@link ProjectsRepository} method.
+     * Saves a given file in the project.
+     *
+     * @param projectId     the project in which the file will be saved.
+     * @param multipartFile the file that will be saved in the project.
+     *                      The file should have a valid content type {@link #fileContentTypeIsAllowed(String)}
      * @return File saved! or throws exception
-	 * 		File with type ___ not supported
-	 * 		or There is already a file with the same name!
+     * File with type ___ not supported
+     * or There is already a file with the same name!
      */
     @RequestMapping(value = "/project/{projectId}/files", method = RequestMethod.POST)
     public String uploadFileToProject(@Valid @PathVariable Long projectId, @RequestParam("file")
-                                        MultipartFile multipartFile){
-		if (this.fileContentTypeIsAllowed(multipartFile.getContentType())){
-			Project project = projectsRepository.findOne(projectId);
-			//TODO: refactor checking the filename for uniqueness
-			for(Doc file : project.getFiles()){
-				if (file.getName().equals(multipartFile.getOriginalFilename())){
-					throw new InvalidDocException("There is already a file with the same name!");
-				}
-			}
-			try{
-				Doc file = new Doc();
-				file.setName(multipartFile.getOriginalFilename());
-				file.setFile(multipartFile.getBytes());
-				file.setType(multipartFile.getContentType());
-				project.getFiles().add(file);
-				projectsRepository.save(project);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return "File saved!";
-		} else{
-			throw new InvalidDocException(
-					String.format("File with type %s not supported", multipartFile.getContentType()));
-		}
+            MultipartFile multipartFile) {
+        if (this.fileContentTypeIsAllowed(multipartFile.getContentType())) {
+            Project project = projectsRepository.findOne(projectId);
+            //TODO: refactor checking the filename for uniqueness
+            for (Doc file : project.getFiles()) {
+                if (file.getName().equals(multipartFile.getOriginalFilename())) {
+                    throw new InvalidDocException("There is already a file with the same name!");
+                }
+            }
+            JSONObject response = new JSONObject();
+            try {
+                Doc file = new Doc();
+                file.setName(multipartFile.getOriginalFilename());
+                file.setFile(multipartFile.getBytes());
+                file.setType(multipartFile.getContentType());
+                project.getFiles().add(file);
+                projectsRepository.save(project);
+                response.put("name", file.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return response.toString();
+        } else {
+            throw new InvalidDocException(
+                    String.format("File with type %s not supported", multipartFile.getContentType()));
+        }
     }
 
-	/**
-	 * GET Serving {@link ProjectsRepository} files method.
-	 * Lists the names of all files currently associated with the project.
-	 * @param projectId the id of the project
-	 * @return a List of all the names of the files currently saved in the project.
+    /**
+     * GET Serving {@link ProjectsRepository} files method.
+     * Lists the names of all files currently associated with the project.
+     *
+     * @param projectId the id of the project
+     * @return a List of all the names of the files currently saved in the project.
      */
-	@RequestMapping(value = "/project/{projectId}/files", method = RequestMethod.GET)
-	public List<String> getAllFileNamesByProjectId(@Valid @PathVariable Long projectId){
-		List<String> fileNames = new LinkedList<>();
-		Project project = projectsRepository.findOne(projectId);
-		fileNames.addAll(project.getFiles().stream().map(Doc::getName).collect(Collectors.toList()));
-		return fileNames;
-	}
+    @RequestMapping(value = "/project/{projectId}/files", method = RequestMethod.GET)
+    public List<String> getAllFileNamesByProjectId(@Valid @PathVariable Long projectId) {
+        List<String> fileNames = new LinkedList<>();
+        Project project = projectsRepository.findOne(projectId);
+        fileNames.addAll(project.getFiles().stream().map(Doc::getName).collect(Collectors.toList()));
+        return fileNames;
+    }
 
-	@RequestMapping(value = "/project/{projectId}/{fileName:.+}", method = RequestMethod.GET)
-	public void getFileFromProjectByName(@PathVariable Long projectId, @PathVariable String fileName, HttpServletResponse response){
-		try{
-			Project project = projectsRepository.findOne(projectId);
-			Doc file = getFileByName(fileName, project);
-			if (file == null){
-				throw new DocNotFoundException("No file found with name: " + fileName);
-			} else{
-				response.setHeader("Content-Disposition", "inline; filename=\""+ file.getName() +"\"");
-				response.setContentType(file.getType());
-				response.setContentLength(file.getFile().length);
-				FileCopyUtils.copy(file.getFile(), response.getOutputStream());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * GET Serving {@link ProjectsRepository} files method.
+     *
+     * @param projectId
+     * @param fileName
+     * @param response
+     */
+    @RequestMapping(value = "/project/{projectId}/{fileName:.+}", method = RequestMethod.GET)
+    public void getFileFromProjectByName(@PathVariable Long projectId, @PathVariable String fileName,
+            HttpServletResponse response) {
+        try {
+            Project project = projectsRepository.findOne(projectId);
+            Doc file = getFileByName(fileName, project);
+            if (this.fileExistsInProject(file, project)) {
+                response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+                response.setContentType(file.getType());
+                response.setContentLength(file.getFile().length);
+                FileCopyUtils.copy(file.getFile(), response.getOutputStream());
+            } else {
+                throw new DocNotFoundException("No file found with name: " + fileName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@RequestMapping(value = "/project/{projectId}/{fileName:.+}", method = RequestMethod.DELETE)
-	public String deleteFileFromProjectByName(@PathVariable Long projectId, @PathVariable String fileName){
-		Project project = projectsRepository.findOne(projectId);
-		Doc file = getFileByName(fileName, project);
-		if (file == null || !project.getFiles().contains(file)){
-			throw new DocNotFoundException("No file found with name: " + fileName);
-		} else{
-			project.getFiles().remove(file);
-			projectsRepository.save(project);
-			return "File "+fileName+" successfully deleted!";
-		}
-	}
+    @RequestMapping(value = "/project/{projectId}/{fileName:.+}", method = RequestMethod.DELETE)
+    public String deleteFileFromProjectByName(@PathVariable Long projectId, @PathVariable String fileName) {
+        Project project = projectsRepository.findOne(projectId);
+        Doc file = getFileByName(fileName, project);
+        JSONObject response = new JSONObject();
+        if (this.fileExistsInProject(file, project)) {
+            project.getFiles().remove(file);
+            projectsRepository.save(project);
+            try {
+                response.put("name", file.getName());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return "File " + fileName + " successfully deleted!";
+        } else {
+            throw new DocNotFoundException("No file found with name: " + fileName);
+        }
+    }
 
-	private Doc getFileByName(String fileName, Project project){
-		Doc tempFile;
-		Doc file = null;
-		Set<Doc> files = project.getFiles();
-		Iterator<Doc> it = files.iterator();
-		while (it.hasNext()){
-			tempFile = it.next();
-			if (fileName.equals(tempFile.getName())){
-				file = tempFile;
-				break;
-			}
-		}
-		return file;
-	}
+    private Doc getFileByName(String fileName, Project project) {
+        Doc tempFile;
+        Doc file = null;
+        Set<Doc> files = project.getFiles();
+        Iterator<Doc> it = files.iterator();
+        while (it.hasNext()) {
+            tempFile = it.next();
+            if (fileName.equals(tempFile.getName())) {
+                file = tempFile;
+                break;
+            }
+        }
+        return file;
+    }
 
-	private boolean fileContentTypeIsAllowed(String contentType){
-		//TODO: add all desirable MimeTypes
-		return contentType.equalsIgnoreCase(MimeTypeUtils.IMAGE_JPEG_VALUE)
-				|| contentType.equalsIgnoreCase(MimeTypeUtils.IMAGE_PNG_VALUE)
-				|| contentType.equalsIgnoreCase(MimeTypeUtils.TEXT_PLAIN_VALUE)
-				|| contentType.equalsIgnoreCase("application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-				|| contentType.equalsIgnoreCase("application/msword")
-				|| contentType.equalsIgnoreCase("application/pdf");
-	}
+    private boolean fileExistsInProject(Doc file, Project project) {
+        return file != null && project.getFiles().contains(file);
+    }
+
+    private boolean fileContentTypeIsAllowed(String contentType) {
+        //TODO: add all desirable MimeTypes
+        return contentType.equalsIgnoreCase(MimeTypeUtils.IMAGE_JPEG_VALUE)
+                || contentType.equalsIgnoreCase(MimeTypeUtils.IMAGE_PNG_VALUE)
+                || contentType.equalsIgnoreCase(MimeTypeUtils.TEXT_PLAIN_VALUE)
+                || contentType
+                .equalsIgnoreCase("application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+                || contentType.equalsIgnoreCase("application/msword")
+                || contentType.equalsIgnoreCase("application/pdf");
+    }
 }
--- src/main/java/paysafe/interns/controllers/TasksRestController.java
+++ src/main/java/paysafe/interns/controllers/TasksRestController.java
@@ -1,25 +1,21 @@
 package paysafe.interns.controllers;
 
-import javax.persistence.EntityNotFoundException;
-import javax.validation.ConstraintViolationException;
-import javax.validation.Valid;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import paysafe.interns.exceptions.InvalidTaskException;
 import paysafe.interns.models.Project;
 import paysafe.interns.models.Task;
 import paysafe.interns.repositories.ProjectsRepository;
 import paysafe.interns.repositories.TasksRepository;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.io.Serializable;
 
 /**
  * A RestController class where we define the endpoint URLs for the Task
@@ -29,110 +25,105 @@ import paysafe.interns.repositories.TasksRepository;
 @RestController
 public class TasksRestController {
 
-	@Autowired
-	private ProjectsRepository projectsRepository;
+    @Autowired
+    private ProjectsRepository projectsRepository;
 
-	@Autowired
-	private TasksRepository tasksRepository;
+    @Autowired
+    private TasksRepository tasksRepository;
 
-	/**
-	 * POST Serves the {@link TaskRepository#save(Task)} method. Saves a task in
-	 * the database related to the provided project id.
-	 * 
-	 * @param task
-	 *            the new Task which will be saved to the database.
-	 * @param projectId
-	 *            the project for which the new task is created.
-	 * @param bindingResult
-	 *            checks the entity validations
-	 * @return Id of the newly created Task or Bad Request with error data
-	 *         object
-	 */
-	@RequestMapping(value = "/tasks/{projectId}", method = RequestMethod.POST)
-	public String addTask(@Valid @RequestBody Task task, @PathVariable Long projectId, BindingResult bindingResult) {
-		try {
-			Project project = projectsRepository.getOne(projectId);
-			task.setProject(project);
-			tasksRepository.saveAndFlush(task);
-		} catch (ConstraintViolationException cve) {
-			throw new InvalidTaskException(cve);
-		}
-		JSONObject response = new JSONObject();
-		try {
-			response.put("id", task.getId());
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
+    /**
+     * POST Serves the {@link TasksRepository#save(Object)}} method. Saves a task in
+     * the database related to the provided project id.
+     *
+     * @param task          the new Task which will be saved to the database.
+     * @param projectId     the project for which the new task is created.
+     * @param bindingResult checks the entity validations
+     * @return Id of the newly created Task or Bad Request with error data
+     * object
+     */
+    @RequestMapping(value = "/tasks/{projectId}", method = RequestMethod.POST)
+    public String addTask(@Valid @RequestBody Task task, @PathVariable Long projectId, BindingResult bindingResult) {
+        try {
+            Project project = projectsRepository.getOne(projectId);
+            task.setProject(project);
+            tasksRepository.saveAndFlush(task);
+        } catch (ConstraintViolationException cve) {
+            throw new InvalidTaskException(cve);
+        }
+        JSONObject response = new JSONObject();
+        try {
+            response.put("id", task.getId());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-		return response.toString();
-	}
+        return response.toString();
+    }
 
-	/**
-	 * GET Serving {@link TasksRepository#findAllByProjectId(projectId)} method
-	 * 
-	 * @param projectId
-	 *            a path parameter by which the method finds a specific tasks
-	 * @return a json string array of all tasks with current projectId or empty
-	 *         array
-	 */
-	@RequestMapping(value = "/tasks/{projectId}", method = RequestMethod.GET)
-	public Iterable<Task> getAllTasksByProjectId(@PathVariable Long projectId) {
+    /**
+     * GET Serving {@link TasksRepository#findAllByProjectId(Long)} method
+     *
+     * @param projectId a path parameter by which the method finds a specific tasks
+     * @return a json string array of all tasks with current projectId or empty
+     * array
+     */
+    @RequestMapping(value = "/tasks/{projectId}", method = RequestMethod.GET)
+    public Iterable<Task> getAllTasksByProjectId(@PathVariable Long projectId) {
 
-		return tasksRepository.findAllByProjectId(projectId);
-	}
+        return tasksRepository.findAllByProjectId(projectId);
+    }
 
-	/**
-	 * GET Serving {@link TasksRepository#getOne(taskId)} method
-	 * 
-	 * @param taskId
-	 *            a path parameter by which the method finds a specific task
-	 * @return task with current taskId or Unable to find task with id current
-	 *         taskId
-	 */
-	@RequestMapping(value = "/task/{taskId}", method = RequestMethod.GET)
-	public String getTaskByTaskId(@PathVariable Long taskId) {
-		try {
-			Task task = null;
-			try {
-				task = tasksRepository.getOne(taskId);
-			} catch (ConstraintViolationException cve) {
-				throw new InvalidTaskException(cve);
-			}
-			JSONObject response = new JSONObject();
-			try {
-				response.put("id", task.getId());
-				response.put("name", task.getName());
-				response.put("duration", task.getDuration());
-				response.put("comment", task.getComment());
-				response.put("projectId", task.getProject().getId());
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
+    /**
+     * GET Serving {@link TasksRepository#getOne(Serializable)} method
+     *
+     * @param taskId a path parameter by which the method finds a specific task
+     * @return task with current taskId or Unable to find task with id current
+     * taskId
+     */
+    @RequestMapping(value = "/task/{taskId}", method = RequestMethod.GET)
+    public String getTaskByTaskId(@PathVariable Long taskId) {
+        try {
+            Task task = null;
+            try {
+                task = tasksRepository.getOne(taskId);
+            } catch (ConstraintViolationException cve) {
+                throw new InvalidTaskException(cve);
+            }
+            JSONObject response = new JSONObject();
+            try {
+                response.put("id", task.getId());
+                response.put("name", task.getName());
+                response.put("duration", task.getDuration());
+                response.put("comment", task.getComment());
+                response.put("projectId", task.getProject().getId());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return response.toString();
+        } catch (EntityNotFoundException enfe) {
+            throw new InvalidTaskException("Task not found");
+        }
+    }
 
-			return response.toString();
-		} catch (EntityNotFoundException enfe) {
-			return "Unable to find task with id " + taskId;
-		}
-	}
-
-	/**
-	 * DELETE Serving {@link TasksRepository#delete(taskForDeleted)} method
-	 * 
-	 * @param taskId
-	 *            a path parameter by which the method finds a specific task
-	 * @return Task with current taskId deleted or Unable to find task with
-	 *         current taskId.
-	 */
-	@RequestMapping(value = "/task/{taskId}", method = RequestMethod.DELETE)
-	public String deleteTaskByProjectId(@PathVariable Long taskId) {
-		try {
-			Task taskForDeleted = tasksRepository.getOne(taskId);
-			tasksRepository.delete(taskForDeleted);
-
-			return "Task " + taskId + " deleted.";
-
-		} catch (JpaObjectRetrievalFailureException jorfe) {
-			return "Unable to find task with id " + taskId;
-		}
-	}
+    /**
+     * DELETE Serving {@link TasksRepository#delete(Serializable)} method
+     *
+     * @param taskId a path parameter by which the method finds a specific task
+     * @return Task with current taskId deleted or Unable to find task with
+     * current taskId.
+     */
+    @RequestMapping(value = "/task/{taskId}", method = RequestMethod.DELETE)
+    public String deleteTaskByTaskId(@PathVariable Long taskId) {
+        JSONObject response = new JSONObject();
+        try {
+            Task taskForDeleted = tasksRepository.getOne(taskId);
+            tasksRepository.delete(taskForDeleted);
+            response.put("id", taskForDeleted.getId());
+            return response.toString();
+        } catch (JpaObjectRetrievalFailureException jorfe) {
+            throw new InvalidTaskException("No task found with id " + taskId);
+        } catch (JSONException e) {
+            throw new InvalidTaskException("Error deleting task");
+        }
+    }
 }
